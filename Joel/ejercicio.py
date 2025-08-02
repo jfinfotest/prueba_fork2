f729761fc13f9ee48e3d619006b2bc4d3a18236d
joelmarinos8@gmail.com <joelmarinos8@gmail.com>
2025-08-01 20:19:43 -0500

    Actualización

---
+++ Joel/ejercicio.py
@@ -0,0 +1,7 @@
+# Escribir un programa que almacene las materias de un grupo (por ejemplo Matemáticas,
+#  Física, Química, Sociales y Español) en una lista y la muestre por pantalla.
+
+materias = ["Matemáticas", "Física", "Química", "Sociales", "Español"]
+for materia in materias:
+    print(materia)
+
